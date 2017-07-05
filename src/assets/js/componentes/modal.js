e19ebd78f5419d1c9325e4106e38bf598ee64f7d
stephHiyagon <mv.hiyagon@gmail.com>
2017-07-05 17:54:22 -0500

    solo faltan comentarios y css

--- src/assets/js/componentes/modal.js
+++ src/assets/js/componentes/modal.js
@@ -7,6 +7,8 @@ const Modal=(update)=>{
   var publico;
   var site;
   var faviconI;
+  var principal;
+  var describe;
 
   console.log(state.site);
   console.log(state.titulo);
@@ -15,32 +17,39 @@ const Modal=(update)=>{
     titulo=state.data.note;
     publico="Sin información";
     faviconI="http://www.imagen.com.mx/assets/img/imagen_share.png";
-
+    principal=" ";
+    describe= " ... "
   }
 
   if(state.titulo!=undefined){
 
     titulo=state.titulo.name;
-    publico=state.titulo.published_at;
+    if(state.titulo.published_at!=null){
+      publico=state.titulo.published_at;
+    }else{
+      publico="sin información";
+    };
     faviconI=state.data.metadata.link.favicon;
+    principal=state.data.metadata.link.title;
+    describe=state.titulo.description;
   }
 
   if(state.site==undefined){
     site=" ";
-    $('.proced--site').css("color","white");
-    // favicon=" ";
   }
 
   if(state.site!=undefined){
-    site=state.site.site_name;
-    // favicon=state.site.favicon;
+    site= 'Artículo de ' + state.site.site_name;
   }
 
   console.log(faviconI);
+  console.log(principal);
+  console.log(describe);
   const section= $('<section class="modal"></section>');
   const item=$('<div class="item"></div>');
   const close=$('<span id="close">X</span>');
   const item__stack=$('<div class="item__stack"></div>');
+  const titlePrincipal=$('<div class="item__stack--titlePrincipal">'+principal+'</div>');
   const image=$('<img src="'+state.image+'" class="item__stack--image" alt="imagen" />');
   const item__info=$('<div class="item__info"></div>');
   const pin__count=$('<div class="item__info--pin-count"></div>');
@@ -49,8 +58,9 @@ const Modal=(update)=>{
   const h3=$('<h3 class="item-title"></h3>');
   const subt=$('<div class="item-subtl"></div>');
   const proced=$('<div class="item__info--proced"></div>');
-  const sites=$('<div class="proced--site"> Artículo de '+site+'</div>');
+  const sites=$('<div class="proced--site"> '+site+'</div>');
   const favicon=$('<img src="'+faviconI+'" alt="favicon" width="15%">');
+  const desc=$('<p class="item__info--descrip">'+describe+'</p>');
   const publicado=$('<div class="item__info--publicar">Publicado</div>');
   const fecha=$('<div class="publicar--fecha">'+publico+'</div>');
   const user=$('<div class="item__info--user"></div>');
@@ -63,6 +73,7 @@ const Modal=(update)=>{
   section.append(item);
   item.append(close);
   item.append(item__stack);
+  item__stack.append(titlePrincipal);
   item__stack.append(image);
   item.append(item__info);
   item__info.append(pin__count);
@@ -73,6 +84,7 @@ const Modal=(update)=>{
   item__info.append(proced);
   proced.append(favicon);
   proced.append(sites);
+  item__info.append(desc);
   item__info.append(publicado);
   publicado.append(fecha);
   item__info.append(user);
@@ -88,8 +100,7 @@ const Modal=(update)=>{
   });
 
   return section;
-//
-// },8000);
+
 
 
 
